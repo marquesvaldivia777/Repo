7b55ba78fa6a14a57d83ca720ed1778d5718264c
marquesvaldivia777 <marques.valdivia@csuglobal.edu>
2025-03-23 09:28:06 -0700

    First Commit Module 1

---
+++ Module1CT/src/ModuleOne.java
@@ -0,0 +1,15 @@
+
+public class ModuleOne {
+
+	public static void main(String[] args) {
+		
+		System.out.println("First Name: Marques");
+		System.out.println("Last Name: Valdivia");
+		System.out.println("Address: 5555 Lincoln Ave apt 128, ");
+		System.out.println("City/State: Cypress, CA");
+		System.out.print("Zip Code: 90000");
+				
+
+	}
+
+}
